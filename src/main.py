b93273f6e4cd1b8bc029ffcb2efcc2ba2b4d70a3
Axel <nigthmaresans2@gmail.com>
2024-09-21 14:27:19 -0600

    yes

--- src/main.py
+++ src/main.py
@@ -6,18 +6,22 @@ class Args:
     send: bool
     recieve: bool
 
+    address: str
     port: int
 
 parser = argparse.ArgumentParser(prog="sdt", description="A program for transfering files through sockets")
 
-parser.add_argument("-s", "--send", action="store_true", help="Sends something to a server.")
+parser.add_argument("-s", "--send", action="store", help="Sends something to a server.")
 parser.add_argument("-r", "--recieve", action="store_true", help="Recieve a file")
 
+parser.add_argument("-a", "--address", default="127.0.0.1", help="The address of the server to create/connect")
 parser.add_argument("-p", "--port", default="5050", help="The port of the server or the device to connect")
 
 args: Args = parser.parse_args()
 
 if args.send:
-    Client().send_file("test.file")
+    c = Client(args.address, args.port)
+    c.send_file(args.send)
+
 if args.recieve:
     Server().recieve()
